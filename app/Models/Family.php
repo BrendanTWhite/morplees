3364ac626469979bf3de8146cb846dc1f5a7a037
Brendan T White <brendan@thespia.com>
2021-12-29 09:40:11 +1030

    Can now create Families

--- app/Models/Family.php
+++ app/Models/Family.php
@@ -9,6 +9,14 @@ class Family extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
 
 
 
