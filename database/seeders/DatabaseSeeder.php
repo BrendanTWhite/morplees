01022e3fa03f7cd5fcff4edf442f1d5f25760854
Brendan T White <brendan@thespia.com>
2021-12-28 15:39:27 +1030

    User catchup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
 	    $this->call([
 	        FamilySeeder::class,
-	        //UserSeeder::class,
+	        UserSeeder::class,
 	        ShopSeeder::class,
 	        ProductSeeder::class,
 	        RecipeSeeder::class,
