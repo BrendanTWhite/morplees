5f927719c8bb33862ff3a7ddeb6efe6d7af0c399
Brendan T White <brendan@thespia.com>
2021-12-31 15:40:14 +1030

    slight cleanup

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -8,6 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Recipe extends Model
 {
     use HasFactory;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
 
 
 
