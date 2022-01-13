ec81a18313d5654d7de7c3d62d1436b88833f8ee
Brendan T White <brendan@thespia.com>
2022-01-13 20:04:45 +1030

    Fixing all other places where the password is saved, so that they all save the plaintext password - and it's then hashed (once) by the setter on the model

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -8,7 +8,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+//use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 
 class RegisteredUserController extends Controller
@@ -42,7 +42,7 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => $request->password, // Hash::make($request->password),
         ]);
 
         event(new Registered($user));
