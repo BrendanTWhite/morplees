277804d662bb4ef8564a3ae44c49864596f3bb60
Brendan T White <brendan@thespia.com>
2022-01-24 20:31:11 +1030

    don't really want to show ingredients on a product

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -49,7 +49,7 @@ class ProductResource extends Resource
     public static function getRelations(): array
     {
         return [
-            RelationManagers\IngredientsRelationManager::class,
+            //RelationManagers\IngredientsRelationManager::class,
         ];
     }
 
