bc64fe82e071f6bacbbd46e9f4dd8ecc94bc488f
Brendan T White <brendan@thespia.com>
2022-01-24 20:59:10 +1030

    Switched off Policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,18 +23,18 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [ 
-        Family::class       => FamilyPolicy::class,
-        User::class         => UserPolicy::class,
-        Shop::class         => ShopPolicy::class,
-        Product::class      => ProductPolicy::class,
+        // Family::class       => FamilyPolicy::class,
+        // User::class         => UserPolicy::class,
+        // Shop::class         => ShopPolicy::class,
+        // Product::class      => ProductPolicy::class,
 
-        Recipe::class       => RecipePolicy::class,
-        Step::class         => StepPolicy::class,
-        Ingredient::class   => IngredientPolicy::class,
+        // Recipe::class       => RecipePolicy::class,
+        // Step::class         => StepPolicy::class,
+        // Ingredient::class   => IngredientPolicy::class,
 
-        ShoppingList::class => ShoppingListPolicy::class,
-        SLRecipe::class     => SLRecipePolicy::class,
-        SLItem::class       => SLItemPolicy::class,
+        // ShoppingList::class => ShoppingListPolicy::class,
+        // SLRecipe::class     => SLRecipePolicy::class,
+        // SLItem::class       => SLItemPolicy::class,
     ];
 
     /**
