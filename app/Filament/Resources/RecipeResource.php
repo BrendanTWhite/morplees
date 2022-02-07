50bef115a37e714ef8adaaa89d278e72271006a4
Brendan T White <brendan@thespia.com>
2022-02-07 13:22:22 +1030

    I hereby declare that the Recipes resource is good enough for 1.0.

--- app/Filament/Resources/RecipeResource.php
+++ app/Filament/Resources/RecipeResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\RecipeResource\Pages;
 use App\Filament\Resources\RecipeResource\RelationManagers;
+use App\Models\Product;
 use App\Models\Recipe;
 use Filament\Forms;
 use Filament\Resources\Form;
@@ -25,12 +26,95 @@ class RecipeResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')->required(),
-                Forms\Components\TextInput::make('prep_time'),
-                Forms\Components\TextInput::make('cook_time'),
-                Forms\Components\TextInput::make('book_reference'),
-                Forms\Components\TextInput::make('url'),
-            ]);
+
+                Forms\Components\Card::make()
+                ->schema([
+
+                    Forms\Components\TextInput::make('name')->required()->autofocus(),
+
+                    Forms\Components\Grid::make()
+                    ->schema([
+                        Forms\Components\TextInput::make('prep_time')->numeric()->postfix('mins'),
+                        Forms\Components\TextInput::make('cook_time')->numeric()->postfix('mins'),
+                        Forms\Components\TextInput::make('book_reference'),
+                    ])->columns(3),
+                    
+                    Forms\Components\TextInput::make('url'),
+
+                    Forms\Components\Grid::make()
+                    ->schema([
+
+                        Forms\Components\HasManyRepeater::make('ingredients')
+                            ->relationship('ingredients')
+                            ->schema([
+                                Forms\Components\Hidden::make('sequence')->default(999),
+
+                                // Forms\Components\TextInput::make('sequence')->numeric()->label('')->prefix('#')
+                                //     ->columnSpan([
+                                //         'md' => 3,
+                                //     ]),
+
+                                Forms\Components\TextInput::make('quantity')->label('')
+                                    ->columnSpan([
+                                        'md' => 3,
+                                    ]),
+
+                                //Forms\Components\TextInput::make('product_id'), // not needed
+                                //Forms\Components\TextInput::make('product.name'), // not working
+
+                                Forms\Components\Select::make('product_id')
+                                    //->columns(3)
+                                    ->label('')
+                                    ->options(Product::query()->pluck('name', 'id'))
+                                    ->required()
+                                    ->columnSpan([
+                                        'md' => 7,
+                                    ])
+                                    ->reactive(),
+
+                            ])
+                            ->columns([
+                                'md' => 10,
+                            ])                            
+                            ->createItemButtonLabel('Add Ingredient'),
+
+                        Forms\Components\HasManyRepeater::make('steps')
+                            ->relationship('steps')
+                            ->schema([
+                                Forms\Components\Hidden::make('sequence')->default(999),
+                                // Forms\Components\TextInput::make('sequence')->numeric()->label('')->prefix('#')                                    
+                                // ->columnSpan([
+                                //     'md' => 3,
+                                // ]),
+
+                                Forms\Components\TextInput::make('instructions')->label('')
+                                ->columnSpan([
+                                    'md' => 10,
+                                ]),
+                            ])
+                            ->columns([
+                                'md' => 10,
+                            ])                            
+                            ->createItemButtonLabel('Add Step'),
+
+                    ])
+                    ->columns(2),
+                        
+                ]), // end card
+
+                Forms\Components\Card::make()
+                ->schema([
+                    Forms\Components\Placeholder::make('created_at')
+                        ->label('Created at')
+                        ->content(fn (?Recipe $record): string => $record && $record->created_at ? $record->created_at->diffForHumans() : '-'),
+                    Forms\Components\Placeholder::make('updated_at')
+                        ->label('Last modified at')
+                        ->content(fn (?Recipe $record): string => $record && $record->updated_at ? $record->updated_at->diffForHumans() : '-'),
+                ])->columns(2)
+                ->hidden(fn ($livewire) => $livewire instanceof Pages\CreateRecipe),
+ 
+    
+            ]); // end form
     }
 
     public static function table(Table $table): Table
@@ -57,8 +141,8 @@ class RecipeResource extends Resource
     public static function getRelations(): array
     {
         return [
-            RelationManagers\IngredientsRelationManager::class,
-            RelationManagers\StepsRelationManager::class,
+            // RelationManagers\IngredientsRelationManager::class,
+            // RelationManagers\StepsRelationManager::class,
         ];
     }
 
