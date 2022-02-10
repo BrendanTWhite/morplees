39071b3d2f9bb029102de44889a244d9e222c5f0
Brendan T White <brendan@thespia.com>
2022-02-10 20:32:50 +1030

    Boilerplate for new Pantry resource

---
+++ app/Filament/Resources/PantryResource.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ShoppingListResource\Pages;
+use App\Filament\Resources\ShoppingListResource\RelationManagers;
+use App\Models\ShoppingList;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class ShoppingListResource extends Resource
+{
+    protected static ?string $model = ShoppingList::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListShoppingLists::route('/'),
+            'create' => Pages\CreateShoppingList::route('/create'),
+            'edit' => Pages\EditShoppingList::route('/{record}/edit'),
+        ];
+    }
+}
