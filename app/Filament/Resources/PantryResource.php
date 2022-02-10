9ffce91f6d028f97cf583a75a6f7bd68c663d887
Brendan T White <brendan@thespia.com>
2022-02-10 21:17:34 +1030

    Beginning of Pantry page

--- app/Filament/Resources/PantryResource.php
+++ app/Filament/Resources/PantryResource.php
@@ -2,26 +2,36 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\ShoppingListResource\Pages;
-use App\Filament\Resources\ShoppingListResource\RelationManagers;
+use App\Filament\Resources\PantryResource\Pages;
+use App\Filament\Resources\PantryResource\RelationManagers;
 use App\Models\ShoppingList;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
 
-class ShoppingListResource extends Resource
+class PantryResource extends Resource
 {
     protected static ?string $model = ShoppingList::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-archive';
+    protected static ?string $label = 'Pantry';
+    protected static ?string $pluralLabel = 'Pantry';
+    protected static ?string $slug = 'pantry';
+
+    protected static ?string $navigationGroup = '';
+    public static ?int $navigationSort = 140;
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('created_at')
+                    ->disabled(),
+                Forms\Components\TextInput::make('override_name')
+                    ->placeholder('If not specified, the create date will be used')
             ]);
     }
 
@@ -29,26 +39,37 @@ class ShoppingListResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')->label('Shopping List')->searchable(['override_name']),
+                Tables\Columns\TextColumn::make('slitems_count')->counts('slitems')->label('Total Items'),
+                Tables\Columns\BooleanColumn::make('active')
+                ->trueIcon('heroicon-o-check')
+                ->trueColor('success')
+                ->falseIcon('heroicon-o-minus-sm')
+                ->falseColor('secondary')
+                ->action(function (ShoppingList $record): void {
+                    $record->toggleActive();
+                }),
+
             ])
-            ->filters([
-                //
-            ]);
+            ->actions([]) 
+            ;
     }
 
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\SLRecipesRelationManager::class,
         ];
     }
 
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListShoppingLists::route('/'),
-            'create' => Pages\CreateShoppingList::route('/create'),
-            'edit' => Pages\EditShoppingList::route('/{record}/edit'),
+            'index' => Pages\ListPantrys::route('/'),
+            'create' => Pages\CreatePantry::route('/create'),
+            'view' => Pages\ViewPantry::route('/{record}'),
+            'edit' => Pages\EditPantry::route('/{record}/edit'),
         ];
     }
+
 }
