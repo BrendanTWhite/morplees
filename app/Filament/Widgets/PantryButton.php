dfbeb544c0824bddd334f5db533a3002aa28239a
Brendan T White <brendan@thespia.com>
2022-04-15 08:10:32 +0930

    artisan make:filament-widget PantryButton

---
+++ app/Filament/Widgets/PantryButton.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class PantryButton extends Widget
+{
+    protected static string $view = 'filament.widgets.pantry-button';
+}
