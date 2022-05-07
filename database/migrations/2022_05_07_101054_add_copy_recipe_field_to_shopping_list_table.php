517b948916e032018e74ecd87232d52b2fa94dfb
Brendan T White <brendan@thespia.com>
2022-05-07 19:41:42 +0930

    artisan make:migration add_copy_recipe_field_to_shopping_list_table

---
+++ database/migrations/2022_05_07_101054_add_copy_recipe_field_to_shopping_list_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCopyRecipeFieldToShoppingListTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shopping_list', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shopping_list', function (Blueprint $table) {
+            //
+        });
+    }
+}
