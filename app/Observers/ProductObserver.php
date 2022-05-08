7439d9ee8dd351f1b217d295d0a1864393b65342
Brendan T White <brendan@thespia.com>
2022-05-08 21:02:13 +0930

    artisan make:observer ProductObserver --model=Product  - and then remove unwanted ones and add wanted ones

---
+++ app/Observers/ProductObserver.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Product;
+
+class ProductObserver
+{
+
+    /**
+     * Handle the Product "creating" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function creating(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the Product "created" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function created(Product $product)
+    {
+        //
+    }
+
+}
