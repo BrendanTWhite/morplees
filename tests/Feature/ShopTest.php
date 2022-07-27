c1b6635fef7cd5ce9d93c9a14f9da09ff71864fe
Brendan T White <brendan@thespia.com>
2022-07-27 21:34:27 +0930

    First tests created

--- tests/Feature/ShopTest.php
+++ tests/Feature/ShopTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class ShopTest extends TestCase
 {
@@ -13,10 +14,37 @@ class ShopTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_shops_list_can_be_rendered()
     {
-        $response = $this->get('/');
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->get('/shops/');
 
         $response->assertStatus(200);
+        $this->assertAuthenticated();
+        $response->assertSee('Shops');
+        $response->assertSee('New shop');
     }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_add_shop_page_can_be_rendered()
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->get('/shops/');
+
+        $response->assertStatus(200);
+        $this->assertAuthenticated();
+        $response->assertSee('Shops');
+        $response->assertSee('New shop');
+    }
+
 }
