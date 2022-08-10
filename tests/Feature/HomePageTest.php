67761233fae95b75f690c0c33d7b95d031e1ed69
Brendan T White <brendan@thespia.com>
2022-08-10 20:29:59 +0930

    renamed again...

--- tests/Feature/HomePageTest.php
+++ tests/Feature/HomePageTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\User;
 
-class ExampleTest extends TestCase
+class HomePageTest extends TestCase
 {
     /**
      * A basic test example.
