fda8ae186816828781d375275015a126c2e4c837
Brendan T White <brendan@thespia.com>
2022-09-04 08:00:41 +0930

    another small tweak

--- app/Console/Commands/DatabaseMask/Backup.php
+++ app/Console/Commands/DatabaseMask/Backup.php
@@ -5,7 +5,6 @@ namespace App\Console\Commands\DatabaseMask;
 use App\Services\DatabaseMask;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\App;
-use Spatie\DbSnapshots\Snapshot;
 use Spatie\DbSnapshots\Helpers\Format;
 
 class Backup extends Command
@@ -65,7 +64,7 @@ class Backup extends Command
 
         $size = Format::humanReadableSize($backup->size());
         
-        $this->info("Successfully backed up this '$environment' environment to file '$backup->fileName' (size $size).");
+        $this->info("Successfully backed up the database to file '$backup->fileName' (size $size).");
 
         return Command::SUCCESS;
     }
