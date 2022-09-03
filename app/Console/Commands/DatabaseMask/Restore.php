824765e7c0a1b0bf2d3524129ee2babaf8cb2f03
Brendan T White <brendan@thespia.com>
2022-09-04 08:45:04 +0930

    progress commit

--- app/Console/Commands/DatabaseMask/Restore.php
+++ app/Console/Commands/DatabaseMask/Restore.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands\DatabaseMask;
 
 use Illuminate\Console\Command;
+use Spatie\DbSnapshots\SnapshotRepository;
+use Illuminate\Support\Facades\App;
 
 class Restore extends Command
 {
@@ -37,8 +39,89 @@ class Restore extends Command
      */
     public function handle()
     {
-        $filename = $this->argument('filename') ?? $this->ask('What filename?');
-        $this->info("Running DatabaseMask's Restore from $filename!");
+        $this->info("Running DatabaseMask Restore");
+
+        $environment = App::environment();
+        if ($environment == 'production') {
+                $this->info("DBM will not restore to a '$environment' environment.");
+                return Command::INVALID;
+        } 
+         
+        $this->warn("You are restoring to a '$environment' environment.");
+        if (!$this->confirm('Do you wish to continue?')) {
+            $this->info('Restore cancelled.');
+            return Command::SUCCESS;
+        }
+         
+        // If we don't have a filename specified, give the user a list to choose from
+
+        $filename = $this->argument('filename');
+        if (!$filename) {
+            $this->line('Retreiving list of backups...');
+            $list_of_backups = app(SnapshotRepository::class)->getAll();
+            $this->line('... list retrived.');
+
+            if ($list_of_backups->isEmpty()) {
+                $this->warn('No backups found.');
+                $this->warn('Run `php artisan dbm:backup` from your production environment to create a backup.');
+                return Command::INVALID;
+            }
+
+            define("MOST_RECENT_BACKUP", 0);
+            $filename = $this->choice(
+                'Which backup should we restore?',
+                $list_of_backups->pluck('name')->all(),
+                MOST_RECENT_BACKUP
+            );
+
+            dd($filename);
+        }
+
+
+
+
+
+
+
+
+        $useLatestSnapshot = $this->option('latest') ?: false;
+
+        $name = $useLatestSnapshot
+            ? $backups->first()->name
+            : ($this->argument('name') ?: $this->askForSnapshotName());
+
+        /** @var \Spatie\DbSnapshots\Snapshot $snapshot */
+        $snapshot = app(SnapshotRepository::class)->findByName($name);
+
+        if (! $snapshot) {
+            $this->warn("Snapshot `{$name}` does not exist!");
+
+            return Command::INVALID;
+        }
+
+        if ($this->option('stream') ?: false) {
+            $snapshot->useStream();
+        }
+
+        $snapshot->load($this->option('connection'), (bool) $this->option('drop-tables'));
+
+        $this->info("Snapshot `{$name}` loaded!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         return Command::SUCCESS;
     }
