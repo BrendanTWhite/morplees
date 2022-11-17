fe2d13a48b68a33996049b031bbd2504226eb664
Brendan T White <brendan@thespia.com>
2022-11-17 21:10:35 +1030

    Seems to work. Closes #24.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,6 +52,21 @@
                     {{ __('Log in') }}
                 </x-primary-button>
             </div>
+
+            <!-- Join a Family -->
+            <div class="block mt-4">
+                <a class="justify-start underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('join') }}">
+                    {{ __('Join an existing family ') }}
+                </a>
+            </div>
+
+            <!-- Create a New Family -->
+            <div class="block mt-4">
+                <a class="justify-start underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                    {{ __('Sign up for a new family ') }}
+                </a>
+            </div>
+
         </form>
     </x-auth-card>
 </x-guest-layout>
--- routes/auth.php
+++ routes/auth.php
@@ -15,6 +15,9 @@ Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
+    Route::view('join', 'auth.join')
+                ->name('join');
+
     Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
