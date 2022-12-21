2964ce69c11d9bb44649d772dfd3c5c8458814f0
Brendan T White <brendan@thespia.com>
2022-12-21 14:19:40 +1030

    progress

--- app/Filament/Resources/RecipeResource/Pages/ViewRecipe.php
+++ app/Filament/Resources/RecipeResource/Pages/ViewRecipe.php
@@ -3,11 +3,30 @@
 namespace App\Filament\Resources\RecipeResource\Pages;
 
 use App\Filament\Resources\RecipeResource;
+use App\Models\Recipe;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Pages\Actions;
+use Illuminate\Support\Facades\Log;
+
 
 class ViewRecipe extends ViewRecord
 {
     protected static string $resource = RecipeResource::class;
 
-    //protected static string $view = 'filament.resources.recipe-resource.pages.view-recipe';
+    protected function getActions(): array
+    {
+        return [
+            Actions\Action::make('add_to_menu')->label('Add to Menu')
+            ->action('addToMenu'),
+
+            Actions\EditAction::make(),
+        ];
+    }
+     
+    public function addToMenu(): void
+    {
+        Log::debug($this->record->toJson());
+    }
+
+
 }
