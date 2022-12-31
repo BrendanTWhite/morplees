156c7cbee7afce82d2e3fc737e5ce259f6bf8d15
Brendan T White <brendan@thespia.com>
2022-12-31 12:43:27 +1030

    making progress but does NOT work yet

--- app/Filament/Resources/Pages/EditMorpleesRecord.php
+++ app/Filament/Resources/Pages/EditMorpleesRecord.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Pages;
 
 use Filament\Resources\Pages\EditRecord;
+use Filament\Pages\Actions;
 
 class EditMorpleesRecord extends EditRecord
 {
@@ -10,4 +11,31 @@ class EditMorpleesRecord extends EditRecord
     {
         return $this->getResource()::getUrl('view', ['record' => $this->record->id]);
     }
+
+
+    protected function getFormActions(): array
+    {
+        return [
+
+            Actions\DeleteAction::make(),
+
+            Actions\Action::make('delete')
+                ->action('delete'),
+    
+        ];
+    }
+
+
+    protected function getActions(): array
+    // Action buttons at the top
+    {
+
+        return [ 
+            Actions\DeleteAction::make(),
+            $this->getSaveFormAction(),
+            $this->getCancelFormAction(),
+        ];
+
+    }
+
 }
