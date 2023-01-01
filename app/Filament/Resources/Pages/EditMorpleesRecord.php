f4cd054879026cb0d585fed01f00fa4329629102
Brendan T White <brendan@thespia.com>
2023-01-01 15:00:09 +1030

    Revert "making progress but does NOT work yet"
    This reverts commit 156c7cbee7afce82d2e3fc737e5ce259f6bf8d15.

--- app/Filament/Resources/Pages/EditMorpleesRecord.php
+++ app/Filament/Resources/Pages/EditMorpleesRecord.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\Pages;
 
 use Filament\Resources\Pages\EditRecord;
-use Filament\Pages\Actions;
 
 class EditMorpleesRecord extends EditRecord
 {
@@ -11,31 +10,4 @@ class EditMorpleesRecord extends EditRecord
     {
         return $this->getResource()::getUrl('view', ['record' => $this->record->id]);
     }
-
-
-    protected function getFormActions(): array
-    {
-        return [
-
-            Actions\DeleteAction::make(),
-
-            Actions\Action::make('delete')
-                ->action('delete'),
-    
-        ];
-    }
-
-
-    protected function getActions(): array
-    // Action buttons at the top
-    {
-
-        return [ 
-            Actions\DeleteAction::make(),
-            $this->getSaveFormAction(),
-            $this->getCancelFormAction(),
-        ];
-
-    }
-
 }
