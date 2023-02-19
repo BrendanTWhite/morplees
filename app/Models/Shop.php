cd712073376fcdacb66918c9628cbd376631de5a
Brendan T White <brendan@thespia.com>
2023-02-19 20:55:50 +1030

    57 check all relationships cascade deletes warn about child records (#64)
    * artisan make:migration define_foreign_key_ondelete
    
    * Works! Thanks Stack Overflow!!
    https://stackoverflow.com/a/26826932/2434429
    
    * migrations are working
    
    * Now giving nice message when try to delete a product with one or more related s_l_items
    
    * Now giving nice message when try to delete a shop with one or more related products

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Traits\BelongsToFamily;
+use Filament\Notifications\Notification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -29,6 +30,33 @@ class Shop extends Model
     protected $masked = [
         'name',
     ];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+
+        // If we're trying to delete a shop that is  
+        // being used for a product,
+        // then warn the user and don't delete it.
+        static::deleting(function ($shop) {
+
+            if ( ! $shop->products->isEmpty() ) {
+                Notification::make() 
+                    ->title("Can't delete '$shop->name' as it's been used for a product.")
+                    ->warning()
+                    ->persistent()
+                    ->send(); 
+
+                return false;
+            }
+
+        });
+
+    }
 
     /**
      * Get the family that owns the shop.
