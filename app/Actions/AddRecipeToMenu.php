fbde8b7ab6b45e3bf800efb795d6cbc380859355
Brendan T White <brendan@thespia.com>
2023-09-03 18:39:33 +0930

    progress

--- app/Actions/AddRecipeToMenu.php
+++ app/Actions/AddRecipeToMenu.php
@@ -4,12 +4,14 @@ namespace App\Actions;
 
 use App\Models\Recipe;
 use App\Models\SLRecipe;
+use Illuminate\Support\Facades\Log;
 
 class AddRecipeToMenu
 {
-    public function execute(Recipe $recipe, ?SLRecipe $sLRecipe)
+    public function __invoke(Recipe $recipe, ?SLRecipe $sLRecipe)
     {
 
+        Log::info('batman');
 
     }
 }
