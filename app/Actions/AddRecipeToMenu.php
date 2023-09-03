3addccfa65a57b6ed65d18464a6f7fed0ea59593
Brendan T White <brendan@thespia.com>
2023-09-03 14:58:45 +0930

    I want to move the "Add Recipe to Menu" functionality to an Action, for reusability.

---
+++ app/Actions/AddRecipeToMenu.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Recipe;
+use App\Models\SLRecipe;
+
+class AddRecipeToMenu
+{
+    public function execute(Recipe $recipe, ?SLRecipe $sLRecipe)
+    {
+
+
+    }
+}
